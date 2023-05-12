03a22e4402c5f213723d252f101a8935ceb3df9b
Donnovan Jiles <djiles@mail.sfsu.edu>
2023-05-11 18:49:30 -0700

    create recipe & save functionality

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 // to run cd to server and type npm start
 
 import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
   "mongodb+srv://donny70122:Brianna94501@recipes.n796teo.mongodb.net/recipes?retryWrites=true&w=majority"
